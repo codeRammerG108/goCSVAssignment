95577bd64670d16cf911e48ac88491a368121e2d
codeRammerG108 <Abhishek.Sandliya@triconinfotech.com>
2023-12-12 14:55:36 +0530

    Connection with Database

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
+	DBconnection "github.com/codeRammerG108/goCSVAssignment/db"
+)
+
+var DB *sql.DB
 
 func main() {
+	var err error
 	fmt.Println("Go-Lang Assignment on CSV")
+	DB, err := DBconnection.DBinit()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("\nConnection Established")
+	}
+	defer DB.Close()
+
 }
